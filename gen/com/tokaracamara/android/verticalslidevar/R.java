9ad9372f4e8144ef825804b463327dc693cbce0f
Kolia Morev <kolyuchiy@gmail.com>
2011-08-09 18:28:03 +0400

    add rest of the controls

--- gen/com/tokaracamara/android/verticalslidevar/R.java
+++ gen/com/tokaracamara/android/verticalslidevar/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package ru.denivip.android.video;
+package com.tokaracamara.android.verticalslidevar;
 
 public final class R {
     public static final class attr {
