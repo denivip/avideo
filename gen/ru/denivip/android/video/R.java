ce5524fd1925a844e9b6fbc8ba143ca26470aa1f
Kolia Morev <kolyuchiy@gmail.com>
2011-08-10 13:55:51 +0400

    fix styles

--- gen/ru/denivip/android/video/R.java
+++ gen/ru/denivip/android/video/R.java
@@ -38,59 +38,44 @@ public final class R {
         public static final int progress_horizontal=0x7f020018;
         public static final int progress_indeterminate_horizontal=0x7f020019;
         public static final int progress_secondary=0x7f02001a;
-        public static final int progress_vertical=0x7f02001b;
-        public static final int progressbar_indeterminate1=0x7f02001c;
-        public static final int progressbar_indeterminate2=0x7f02001d;
-        public static final int progressbar_indeterminate3=0x7f02001e;
-        public static final int seek_thumb=0x7f02001f;
-        public static final int seek_thumb_normal=0x7f020020;
-        public static final int seek_thumb_normal_wide=0x7f020021;
-        public static final int seek_thumb_pressed=0x7f020022;
-        public static final int seek_thumb_pressed_wide=0x7f020023;
-        public static final int seek_thumb_selected=0x7f020024;
-        public static final int seek_thumb_selected_wide=0x7f020025;
-        public static final int seek_thumb_wide=0x7f020026;
-        public static final int thumb_background=0x7f020027;
-        public static final int vert_progress=0x7f020028;
-        public static final int vert_progress_secondary=0x7f020029;
-        public static final int vert_progressbar=0x7f02002a;
+        public static final int progressbar_indeterminate1=0x7f02001b;
+        public static final int progressbar_indeterminate2=0x7f02001c;
+        public static final int progressbar_indeterminate3=0x7f02001d;
+        public static final int thumb_background=0x7f02001e;
+        public static final int vert_progress=0x7f02001f;
+        public static final int vert_progress_secondary=0x7f020020;
+        public static final int vert_progressbar=0x7f020021;
     }
     public static final class id {
-        public static final int LinearLayout01=0x7f060000;
-        public static final int ProgressBar01=0x7f060001;
-        public static final int SeekBar01=0x7f060002;
-        public static final int SeekBar02=0x7f060003;
-        public static final int brightness=0x7f060009;
-        public static final int contrast=0x7f060008;
-        public static final int leftButtons=0x7f060007;
-        public static final int mediacontroller_progress=0x7f060006;
-        public static final int mute=0x7f06000e;
-        public static final int pause=0x7f060005;
-        public static final int quality=0x7f06000a;
-        public static final int relativeLayout=0x7f060004;
-        public static final int rightButtons=0x7f06000c;
-        public static final int volume=0x7f06000b;
-        public static final int volumeBar=0x7f06000d;
+        public static final int brightness=0x7f060005;
+        public static final int contrast=0x7f060004;
+        public static final int leftButtons=0x7f060003;
+        public static final int mediacontroller_progress=0x7f060002;
+        public static final int mute=0x7f06000a;
+        public static final int pause=0x7f060001;
+        public static final int quality=0x7f060006;
+        public static final int relativeLayout=0x7f060000;
+        public static final int rightButtons=0x7f060008;
+        public static final int volume=0x7f060007;
+        public static final int volumeBar=0x7f060009;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int media_controller=0x7f030001;
+        public static final int media_controller=0x7f030000;
     }
     public static final class string {
         /**  Button to close error alert when a video cannot be played 
          */
-        public static final int VideoView_error_button=0x7f040005;
+        public static final int VideoView_error_button=0x7f040004;
         /**  Text for error alert when a video container is not valid for progressive download/playback. 
          */
-        public static final int VideoView_error_text_invalid_progressive_playback=0x7f040003;
+        public static final int VideoView_error_text_invalid_progressive_playback=0x7f040002;
         /**  Text for error alert when a video cannot be played. it can be used by any app. 
          */
-        public static final int VideoView_error_text_unknown=0x7f040004;
+        public static final int VideoView_error_text_unknown=0x7f040003;
         /**  Title for error alert when a video cannot be played.  it can be used by any app. 
          */
-        public static final int VideoView_error_title=0x7f040002;
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int VideoView_error_title=0x7f040001;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
         public static final int BigMediaButton=0x7f050000;
@@ -104,128 +89,4 @@ public final class R {
         public static final int SeekBarVertical=0x7f050009;
         public static final int ThumbButton=0x7f050007;
     }
-    public static final class styleable {
-        /** Attributes that can be used with a ProgressBar.
-           <p>Includes the following attributes:</p>
-           <table>
-           <colgroup align="left" />
-           <colgroup align="left" />
-           <tr><th>Attribute</th><th>Description</th></tr>
-           <tr><td><code>{@link #ProgressBar_android_max ru.denivip.android.video:android_max}</code></td><td></td></tr>
-           <tr><td><code>{@link #ProgressBar_android_maxHeight ru.denivip.android.video:android_maxHeight}</code></td><td></td></tr>
-           <tr><td><code>{@link #ProgressBar_android_maxWidth ru.denivip.android.video:android_maxWidth}</code></td><td></td></tr>
-           <tr><td><code>{@link #ProgressBar_android_minHeight ru.denivip.android.video:android_minHeight}</code></td><td></td></tr>
-           <tr><td><code>{@link #ProgressBar_android_minWidth ru.denivip.android.video:android_minWidth}</code></td><td></td></tr>
-           <tr><td><code>{@link #ProgressBar_android_progress ru.denivip.android.video:android_progress}</code></td><td></td></tr>
-           <tr><td><code>{@link #ProgressBar_android_progressDrawable ru.denivip.android.video:android_progressDrawable}</code></td><td></td></tr>
-           <tr><td><code>{@link #ProgressBar_android_secondaryProgress ru.denivip.android.video:android_secondaryProgress}</code></td><td></td></tr>
-           </table>
-           @see #ProgressBar_android_max
-           @see #ProgressBar_android_maxHeight
-           @see #ProgressBar_android_maxWidth
-           @see #ProgressBar_android_minHeight
-           @see #ProgressBar_android_minWidth
-           @see #ProgressBar_android_progress
-           @see #ProgressBar_android_progressDrawable
-           @see #ProgressBar_android_secondaryProgress
-         */
-        public static final int[] ProgressBar = {
-            0x0101011f, 0x01010120, 0x01010136, 0x01010137,
-            0x01010138, 0x0101013c, 0x0101013f, 0x01010140
-        };
-        /**
-          <p>This symbol is the offset where the {@link ru.denivip.android.video.R.attr#android_max}
-          attribute's value can be found in the {@link #ProgressBar} array.
-          @attr name android:android_max
-        */
-        public static final int ProgressBar_android_max = 2;
-        /**
-          <p>This symbol is the offset where the {@link ru.denivip.android.video.R.attr#android_maxHeight}
-          attribute's value can be found in the {@link #ProgressBar} array.
-          @attr name android:android_maxHeight
-        */
-        public static final int ProgressBar_android_maxHeight = 1;
-        /**
-          <p>This symbol is the offset where the {@link ru.denivip.android.video.R.attr#android_maxWidth}
-          attribute's value can be found in the {@link #ProgressBar} array.
-          @attr name android:android_maxWidth
-        */
-        public static final int ProgressBar_android_maxWidth = 0;
-        /**
-          <p>This symbol is the offset where the {@link ru.denivip.android.video.R.attr#android_minHeight}
-          attribute's value can be found in the {@link #ProgressBar} array.
-          @attr name android:android_minHeight
-        */
-        public static final int ProgressBar_android_minHeight = 7;
-        /**
-          <p>This symbol is the offset where the {@link ru.denivip.android.video.R.attr#android_minWidth}
-          attribute's value can be found in the {@link #ProgressBar} array.
-          @attr name android:android_minWidth
-        */
-        public static final int ProgressBar_android_minWidth = 6;
-        /**
-          <p>This symbol is the offset where the {@link ru.denivip.android.video.R.attr#android_progress}
-          attribute's value can be found in the {@link #ProgressBar} array.
-          @attr name android:android_progress
-        */
-        public static final int ProgressBar_android_progress = 3;
-        /**
-          <p>This symbol is the offset where the {@link ru.denivip.android.video.R.attr#android_progressDrawable}
-          attribute's value can be found in the {@link #ProgressBar} array.
-          @attr name android:android_progressDrawable
-        */
-        public static final int ProgressBar_android_progressDrawable = 5;
-        /**
-          <p>This symbol is the offset where the {@link ru.denivip.android.video.R.attr#android_secondaryProgress}
-          attribute's value can be found in the {@link #ProgressBar} array.
-          @attr name android:android_secondaryProgress
-        */
-        public static final int ProgressBar_android_secondaryProgress = 4;
-        /** Attributes that can be used with a SeekBar.
-           <p>Includes the following attributes:</p>
-           <table>
-           <colgroup align="left" />
-           <colgroup align="left" />
-           <tr><th>Attribute</th><th>Description</th></tr>
-           <tr><td><code>{@link #SeekBar_android_thumb ru.denivip.android.video:android_thumb}</code></td><td></td></tr>
-           <tr><td><code>{@link #SeekBar_android_thumbOffset ru.denivip.android.video:android_thumbOffset}</code></td><td></td></tr>
-           </table>
-           @see #SeekBar_android_thumb
-           @see #SeekBar_android_thumbOffset
-         */
-        public static final int[] SeekBar = {
-            0x01010142, 0x01010143
-        };
-        /**
-          <p>This symbol is the offset where the {@link ru.denivip.android.video.R.attr#android_thumb}
-          attribute's value can be found in the {@link #SeekBar} array.
-          @attr name android:android_thumb
-        */
-        public static final int SeekBar_android_thumb = 0;
-        /**
-          <p>This symbol is the offset where the {@link ru.denivip.android.video.R.attr#android_thumbOffset}
-          attribute's value can be found in the {@link #SeekBar} array.
-          @attr name android:android_thumbOffset
-        */
-        public static final int SeekBar_android_thumbOffset = 1;
-        /** Attributes that can be used with a Theme.
-           <p>Includes the following attributes:</p>
-           <table>
-           <colgroup align="left" />
-           <colgroup align="left" />
-           <tr><th>Attribute</th><th>Description</th></tr>
-           <tr><td><code>{@link #Theme_android_disabledAlpha ru.denivip.android.video:android_disabledAlpha}</code></td><td></td></tr>
-           </table>
-           @see #Theme_android_disabledAlpha
-         */
-        public static final int[] Theme = {
-            0x01010033
-        };
-        /**
-          <p>This symbol is the offset where the {@link ru.denivip.android.video.R.attr#android_disabledAlpha}
-          attribute's value can be found in the {@link #Theme} array.
-          @attr name android:android_disabledAlpha
-        */
-        public static final int Theme_android_disabledAlpha = 0;
-    };
 }
