5d21edb69588fc2870d2c6a00676c815f8504459
Kolia Morev <kolyuchiy@gmail.com>
2011-09-03 17:20:26 +0400

    pause video on activity pause, do not reload on orientation change

--- DeniVideoView/src/ru/denivip/android/videoview/Main.java
+++ DeniVideoView/src/ru/denivip/android/videoview/Main.java
@@ -3,6 +3,7 @@ package ru.denivip.android.videoview;
 import ru.denivip.android.video.MediaController;
 import ru.denivip.android.video.VideoView;
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -12,18 +13,39 @@ public class Main extends Activity {
 	
 	private static final String TEST_STREAM_REMOTE = "http://www.denivip.ru/trailer.mp4";
 	
+	private VideoView mVideo;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        VideoView video = (VideoView) findViewById(R.id.videoView);
+        mVideo = (VideoView) findViewById(R.id.videoView);
+        mVideo.requestFocus();
         
         MediaController controller = new MediaController(this);
         
-        video.setMediaController(controller);
-        video.setVideoURI(Uri.parse(TEST_STREAM_LOCAL));
-        video.start();
+        mVideo.setMediaController(controller);
+        mVideo.setVideoURI(Uri.parse(TEST_STREAM_LOCAL));
+        mVideo.start();
     }
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mVideo.suspend();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mVideo.resume();
+		mVideo.start(); // without this pause function will not work after resume
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+	}
 }
